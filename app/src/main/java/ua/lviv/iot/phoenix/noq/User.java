5dd815f972d50660fcc721e9fee7ea3b53627d79
Victor Koropetskyi <viktorua25.03@gmail.com>
2019-04-13 21:08:16 +0300

    .

---
+++ app/src/main/java/ua/lviv/iot/phoenix/noq/User.java
@@ -0,0 +1,13 @@
+package ua.lviv.iot.phoenix.noq;
+
+public class User {
+    private String name;
+    private String email;
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+
+}
